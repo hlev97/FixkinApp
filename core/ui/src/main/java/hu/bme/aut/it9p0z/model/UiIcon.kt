562bac2f258e4ba70072db6051f4f9c70eb692e8
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 00:17:16 +0200

    Add UiIcon

---
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/model/UiIcon.kt
@@ -0,0 +1,20 @@
+package hu.bme.aut.it9p0z.model
+
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+
+
+sealed class UiIcon {
+    data class Image(val image: ImageVector) : UiIcon()
+    data class ImageResource(val id: Int) : UiIcon()
+
+    @Composable
+    fun AsImage() {
+        return when(this) {
+            is Image -> Icon(imageVector = this.image, contentDescription = null)
+            is ImageResource -> Icon(painter = painterResource(id = id), contentDescription = null)
+        }
+    }
+}
