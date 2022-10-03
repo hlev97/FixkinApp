1dba9f2e6da9fa4e8978e5bf5ba0f5e3d014f8e4
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-03 22:38:47 +0200

    Add Dimension

---
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/ui/theme/Dimension.kt
@@ -0,0 +1,18 @@
+package hu.bme.aut.it9p0z.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val dp_xs = 2.dp
+val dp_s = 5.dp
+val dp_m = 10.dp
+val dp_l = 15.dp
+val dp_xl = 30.dp
+val dp_xxl = 35.dp
+
+val stroke_xs = 1.dp
+val stroke_s = 2.dp
+
+val corner_radius_s = 5.dp
+val corner_radius_m = 15.dp
+val corner_radius_l = 25.dp
+
