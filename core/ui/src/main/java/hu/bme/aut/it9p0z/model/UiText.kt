5a254f824cca9e68caa2496867d1e8159461f0a3
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-03 22:30:04 +0200

    Add UIText

---
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/model/UiText.kt
@@ -0,0 +1,15 @@
+package hu.bme.aut.it9p0z.model
+
+import android.content.Context
+
+sealed class UiText {
+    data class DynamicString(val text: String): UiText()
+    data class StringResource(val id: Int): UiText()
+
+    fun asString(context: Context): String {
+        return when(this) {
+            is DynamicString -> text
+            is StringResource -> context.getString(id)
+        }
+    }
+}
