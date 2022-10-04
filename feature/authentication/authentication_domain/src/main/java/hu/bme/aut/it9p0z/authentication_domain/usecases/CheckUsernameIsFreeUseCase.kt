47fc3c62f97318914a9c8256d0c3168adeee3cac
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 03:12:24 +0200

    Add CheckUsernameIsFreeUseCase

---
+++ feature/authentication/authentication_domain/src/main/java/hu/bme/aut/it9p0z/authentication_domain/usecases/CheckUsernameIsFreeUseCase.kt
@@ -0,0 +1,19 @@
+package hu.bme.aut.it9p0z.authentication_domain.usecases
+
+import hu.bme.aut.it9p0z.authentication_data.repository.AuthenticationRepository
+import javax.inject.Inject
+
+class CheckUsernameIsFreeUseCase @Inject constructor(
+    private val authRepository: AuthenticationRepository
+) {
+
+    suspend operator fun invoke(userName: String): Boolean {
+        val response = authRepository.getUsernames()
+        val userNames = response.data
+
+        if (userNames != null) {
+            return userNames.contains(userName)
+        } else throw Exception(response.message)
+    }
+
+}
