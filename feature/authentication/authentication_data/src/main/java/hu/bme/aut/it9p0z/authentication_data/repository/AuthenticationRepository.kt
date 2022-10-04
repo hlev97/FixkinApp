784974d8b3ee350e3caaa0d4eb822a5f79cec6c4
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 02:52:41 +0200

    Add Authentication Repository

---
+++ feature/authentication/authentication_data/src/main/java/hu/bme/aut/it9p0z/authentication_data/repository/AuthenticationRepository.kt
@@ -0,0 +1,50 @@
+package hu.bme.aut.it9p0z.authentication_data.repository
+
+import hu.bme.aut.it9p0z.network.datasource.NetworkDatasource
+import hu.bme.aut.it9p0z.network.dtos.UserDto
+import hu.bme.aut.it9p0z.network.dtos.wrapper.ResponseWrapper
+import hu.bme.aut.it9p0z.preferences.PreferencesDatasource
+import hu.bme.aut.it9p0z.preferences.domain.UserInfo
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class AuthenticationRepository @Inject constructor(
+    private val networkDatasource: NetworkDatasource,
+    private val preferencesDatasource: PreferencesDatasource
+) {
+    suspend fun saveUsername(userName: String) {
+        preferencesDatasource.saveUsername(userName)
+    }
+
+    suspend fun saveFullName(fullName: String) {
+        preferencesDatasource.saveFullName(fullName)
+    }
+
+    suspend fun saveWeight(weight: Double) {
+        preferencesDatasource.saveWeight(weight)
+    }
+
+    suspend fun saveHeight(height: Double) {
+        preferencesDatasource.saveHeight(height)
+    }
+
+    suspend fun saveDiseases(diseases: List<String>) {
+        preferencesDatasource.saveDiseases(diseases)
+    }
+
+    suspend fun saveMedicines(medicines: List<String>) {
+        preferencesDatasource.saveMedicines(medicines)
+    }
+
+    suspend fun saveAverageDermatologyLifeQualityIndex(avgIndex: Double) {
+        preferencesDatasource.saveAverageLifeQualityIndex(avgIndex)
+    }
+
+    suspend fun loadUserInfo(): UserInfo {
+        return preferencesDatasource.loadUserInfo().first()
+    }
+
+    suspend fun registerUser(user: UserDto): ResponseWrapper<UserDto> {
+        return networkDatasource.createUser(user)
+    }
+}
