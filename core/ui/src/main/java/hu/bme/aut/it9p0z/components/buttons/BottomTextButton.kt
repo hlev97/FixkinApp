36e4ef10769dbd26aeb9adb32c8f68160ca0a32e
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 13:58:15 +0200

    Fix BottomTextButton with clip

--- core/ui/src/main/java/hu/bme/aut/it9p0z/components/buttons/BottomTextButton.kt
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/components/buttons/BottomTextButton.kt
@@ -7,12 +7,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import hu.bme.aut.it9p0z.model.UiText
+import hu.bme.aut.it9p0z.ui.theme.dp_l
 import hu.bme.aut.it9p0z.ui.theme.dp_m
 
 @Composable
@@ -27,20 +29,21 @@ fun BottomTextButton(
     Surface(
         modifier = modifier
             .fillMaxWidth()
+            .clip(shape = shape)
             .clickable(
                 enabled = enabled,
                 role = Role.Button,
                 onClick = onClick
             ),
         shape = shape,
-        color = MaterialTheme.colorScheme.surfaceVariant
+        color = MaterialTheme.colorScheme.primary
     ) {
         Text(
             text = uiText.asString(context),
             textAlign = TextAlign.Center,
-            modifier = Modifier.fillMaxWidth().padding(dp_m),
+            modifier = Modifier.fillMaxWidth().padding(dp_l),
             maxLines = 1,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            color = MaterialTheme.colorScheme.onPrimary,
             style = MaterialTheme.typography.labelLarge
         )
     }
