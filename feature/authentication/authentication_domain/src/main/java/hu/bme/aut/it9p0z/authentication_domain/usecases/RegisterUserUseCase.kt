7886d67f6b1ee7d829c334f28981be3f0d3461ac
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 03:12:35 +0200

    Add RegisterUserUseCase

---
+++ feature/authentication/authentication_domain/src/main/java/hu/bme/aut/it9p0z/authentication_domain/usecases/RegisterUserUseCase.kt
@@ -0,0 +1,22 @@
+package hu.bme.aut.it9p0z.authentication_domain.usecases
+
+import hu.bme.aut.it9p0z.authentication_data.repository.AuthenticationRepository
+import hu.bme.aut.it9p0z.model.userinfo.UserInfoModel
+import hu.bme.aut.it9p0z.network.dtos.asUserDto
+import hu.bme.aut.it9p0z.network.dtos.asUserInfoModel
+import hu.bme.aut.it9p0z.preferences.domain.asUserInfoModel
+import javax.inject.Inject
+
+class RegisterUserUseCase @Inject constructor(
+    private val authRepository: AuthenticationRepository
+) {
+    suspend operator fun invoke(): UserInfoModel {
+        val user = authRepository.loadUserInfo().asUserInfoModel().asUserDto()
+        val response = authRepository.registerUser(user)
+        val data = response.data
+
+        if (data != null) {
+            return data.asUserInfoModel()
+        } else throw Exception(response.message)
+    }
+}
