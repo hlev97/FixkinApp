33a79200330c68f2f6cd2fc5ea334e97ec6a8a44
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-04 02:44:33 +0200

    Add :feature:authentication:authentication_data module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,4 +81,7 @@ dependencies {
     implementation(project(Modules.coreModel))
     implementation(project(Modules.coreNetwork))
     implementation(project(Modules.corePreferences))
+
+    implementation(project(Modules.featureAuthenticationData))
+
 }
---
+++ feature/authentication/authentication_data/build.gradle.kts
@@ -0,0 +1,9 @@
+apply {
+    from("$rootDir/base-module.gradle")
+}
+
+dependencies {
+    "implementation"(project(Modules.coreModel))
+    "implementation"(project(Modules.coreNetwork))
+    "implementation"(project(Modules.corePreferences))
+}
