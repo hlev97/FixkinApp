c266507a7423657e28f0bf3cd502e041ae95f8fe
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-07 13:43:59 +0200

    Add new api requests to FixkinApi to manage condition log related operations

--- core/network/src/main/java/hu/bme/aut/it9p0z/network/api/FixkinApi.kt
+++ core/network/src/main/java/hu/bme/aut/it9p0z/network/api/FixkinApi.kt
@@ -1,10 +1,15 @@
 package hu.bme.aut.it9p0z.network.api
 
+import hu.bme.aut.it9p0z.network.dtos.ConditionLogDto
 import hu.bme.aut.it9p0z.network.dtos.UserDto
 import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface FixkinApi {
 
@@ -12,8 +17,34 @@ interface FixkinApi {
     suspend fun createUser(user: UserDto): Response<UserDto>
 
     @GET("user/me")
-    suspend fun getUser(@Header("Authorization") credentials: String): Response<UserDto>
+    suspend fun getUser(
+        @Header("Authorization") credentials: String
+    ): Response<UserDto>
 
     @GET("user/all/usernames")
     suspend fun getAllUsernames(): Response<List<String>>
+
+    @POST("skin_condition_logs/create")
+    suspend fun createConditionLog(
+        @Header("Authorization") credentials: String,
+        @Body log: ConditionLogDto
+    ): Response<ConditionLogDto>
+
+    @PUT("skin_condition_logs/{scLogId}/update")
+    suspend fun updateConditionLog(
+        @Header("Authorization") credentials: String,
+        @Path("scLogId") scLogId: Int,
+        @Body log: ConditionLogDto
+    ): Response<ConditionLogDto>
+
+    @GET("skin_condition_logs/all/me")
+    suspend fun getAllConditionLogs(
+        @Header("Authorization") credentials: String,
+    ): Response<List<ConditionLogDto>>
+
+    @DELETE("skin_condition_logs/{scLogId}/update")
+    suspend fun deleteConditionLog(
+        @Header("Authorization") credentials: String,
+        @Path("scLogId") scLogId: Int,
+    )
 }
