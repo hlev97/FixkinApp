8b70073bb849342957483a07d32f965ec34456a5
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-07 13:47:12 +0200

    Add ConditionLogDataModule for Hilt

---
+++ feature/conditionlog/conditionlog_data/src/main/java/hu/bme/aut/it9p0z/conditionlog_data/di/ConditionLogDataModule.kt
@@ -0,0 +1,27 @@
+package hu.bme.aut.it9p0z.conditionlog_data.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.it9p0z.conditionlog_data.repository.ConditionLogRepository
+import hu.bme.aut.it9p0z.database.datasource.DatabaseDatasource
+import hu.bme.aut.it9p0z.network.datasource.NetworkDatasource
+import hu.bme.aut.it9p0z.preferences.PreferencesDatasource
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ConditionLogDataModule {
+
+    @Provides
+    @Singleton
+    fun provideConditionLogRepository(
+        preferencesDatasource: PreferencesDatasource,
+        networkDatasource: NetworkDatasource,
+        databaseDatasource: DatabaseDatasource
+    ): ConditionLogRepository = ConditionLogRepository(
+        preferencesDatasource,networkDatasource,databaseDatasource
+    )
+
+}
