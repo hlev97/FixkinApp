e085d239bfb4b2ceff97f71e4664422e96f12dda
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-08 21:46:15 +0200

    Add modifier input for UiIcon AsImage function

--- core/ui/src/main/java/hu/bme/aut/it9p0z/ui/model/UiIcon.kt
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/ui/model/UiIcon.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.it9p0z.ui.model
 
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 
@@ -11,10 +12,18 @@ sealed class UiIcon {
     data class ImageResource(val id: Int) : UiIcon()
 
     @Composable
-    fun AsImage() {
+    fun AsImage(modifier: Modifier = Modifier) {
         return when(this) {
-            is Image -> Icon(imageVector = this.image, contentDescription = null)
-            is ImageResource -> Icon(painter = painterResource(id = id), contentDescription = null)
+            is Image -> Icon(
+                imageVector = this.image,
+                contentDescription = null,
+                modifier = modifier
+            )
+            is ImageResource -> Icon(
+                painter = painterResource(id = id),
+                contentDescription = null,
+                modifier = modifier
+            )
         }
     }
 }
