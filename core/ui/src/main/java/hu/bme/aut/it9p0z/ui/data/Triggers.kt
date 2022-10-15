046b758f7ccdfd88bb9349c601a62a307448de07
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-15 14:36:20 +0200

    Add extension functions to Triggers to get UiChips from ConditionLogModel

--- core/ui/src/main/java/hu/bme/aut/it9p0z/ui/data/Triggers.kt
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/ui/data/Triggers.kt
@@ -1,50 +1,121 @@
 package hu.bme.aut.it9p0z.ui.data
 
+import android.content.Context
+import hu.bme.aut.it9p0z.model.conditionlog.ConditionLogModel
 import hu.bme.aut.it9p0z.ui.R
 import hu.bme.aut.it9p0z.ui.model.UiChip
+import hu.bme.aut.it9p0z.ui.model.UiChip.Companion.toUiChipState
 import hu.bme.aut.it9p0z.ui.model.UiIcon
 import hu.bme.aut.it9p0z.ui.model.UiText
 
-sealed class Trigger(val name: UiText, val icon: UiIcon? = null) {
+sealed class UiTrigger(val name: UiText, val icon: UiIcon? = null) {
 
-    object Gluten: Trigger(name = UiText.StringResource(id = R.string.trigger_name_gluten))
-    object Lactose: Trigger(name = UiText.StringResource(id = R.string.trigger_name_lactose))
-    object Milk: Trigger(name = UiText.StringResource(id = R.string.trigger_name_milk))
-    object Egg: Trigger(name = UiText.StringResource(id = R.string.trigger_name_egg))
-    object Soy: Trigger(name = UiText.StringResource(id = R.string.trigger_name_soy))
-    object NightShade: Trigger(name = UiText.StringResource(id = R.string.trigger_name_nightshade))
-    object CitrusFruit: Trigger(name = UiText.StringResource(id = R.string.trigger_name_citrus))
-    object FastFood: Trigger(name = UiText.StringResource(id = R.string.trigger_name_fastfood))
-    object FattyFood: Trigger(name = UiText.StringResource(id = R.string.trigger_name_fattyfood))
-    object Alcohol: Trigger(name = UiText.StringResource(id = R.string.trigger_name_alcohol))
+    object Wheat: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_wheat),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_wheat_24)
+    )
+    object Milk: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_milk),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_milk_24)
+    )
+    object Egg: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_egg),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_egg_24)
+    )
+    object SeaFood: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_seafood),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_seafood_24)
+    )
+    object NightShade: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_nightshade),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_nightshade_24)
+    )
+    object Soy: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_soy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_soy_24)
+    )
+    object CitrusFruit: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_citrus),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_citrus_fruit_24)
+    )
+    object FastFood: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_fastfood),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_fast_food_24)
+    )
+    object FattyFood: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_fattyfood),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_fatty_food_24)
+    )
+    object Alcohol: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_alcohol),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_alcohol_24)
+    )
 
-    object Hot: Trigger(name = UiText.StringResource(id = R.string.trigger_name_hot))
-    object Dry: Trigger(name = UiText.StringResource(id = R.string.trigger_name_dry))
-    object Cold: Trigger(name = UiText.StringResource(id = R.string.trigger_name_cold))
-    object Rainy: Trigger(name = UiText.StringResource(id = R.string.trigger_name_rainy))
-    object Windy: Trigger(name = UiText.StringResource(id = R.string.trigger_name_windy))
-    object Snowy: Trigger(name = UiText.StringResource(id = R.string.trigger_name_snowy))
-    object ColdFront: Trigger(name = UiText.StringResource(id = R.string.trigger_name_coldfront))
-    object WarmFront: Trigger(name = UiText.StringResource(id = R.string.trigger_name_warmfront))
+    object Hot: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_hot),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_hot_24)
+    )
+    object Dry: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_dry),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_dry_24)
+    )
+    object Cold: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_cold),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_cold_24)
+    )
+    object Rainy: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_rainy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_rainy_24)
+    )
+    object Windy: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_windy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_windy_24)
+    )
+    object Snowy: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_snowy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_snowy_24)
 
-    object Anxiety: Trigger(name = UiText.StringResource(id = R.string.trigger_name_anxiety))
-    object Depression: Trigger(name = UiText.StringResource(id = R.string.trigger_name_depression))
-    object Insomnia: Trigger(name = UiText.StringResource(id = R.string.trigger_name_insomnia))
+    )
 
-    object Medicine: Trigger(name = UiText.StringResource(id = R.string.trigger_name_medicine))
-    object Infection: Trigger(name = UiText.StringResource(id = R.string.trigger_name_infection))
-    object Sweat: Trigger(name = UiText.StringResource(id = R.string.trigger_name_sweat))
-    object Smoking: Trigger(name = UiText.StringResource(id = R.string.trigger_name_smoking))
+    object Anxiety: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_anxiety),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_anxiety_24)
+    )
+    object Depression: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_depression),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_depression_24)
+    )
+    object Insomnia: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_insomnia),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_insomnia_24)
+    )
+
+    object Medicine: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_medicine),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_medicine_24)
+    )
+    object Infection: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_infection),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_infection_24)
+    )
+    object Sweat: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_sweat),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_sweat_24)
+    )
+    object Smoking: UiTrigger(
+        name = UiText.StringResource(id = R.string.trigger_name_smoking),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_smoking_24)
+    )
 
     companion object {
 
         private val foodTriggers = listOf(
-            Gluten,
+            Wheat,
             Milk,
-            Lactose,
             Egg,
-            Soy,
+            SeaFood,
             NightShade,
+            Soy,
             CitrusFruit,
             FastFood,
             FattyFood,
@@ -65,8 +136,6 @@ sealed class Trigger(val name: UiText, val icon: UiIcon? = null) {
             Rainy,
             Windy,
             Snowy,
-            ColdFront,
-            WarmFront
         )
 
         val weatherTriggerChips = weatherTriggers.map {
@@ -103,4 +172,72 @@ sealed class Trigger(val name: UiText, val icon: UiIcon? = null) {
             )
         }
     }
+}
+
+fun ConditionLogModel.getFoodTriggerUiChips(context: Context): List<UiChip> {
+    val uiChips = UiTrigger.foodTriggerChips
+    val triggers = this.foodTriggers.map {
+        UiChip(
+            icon = null,
+            label = UiText.DynamicString(it.key),
+            state = it.value.toUiChipState()
+        )
+    }
+    uiChips.forEachIndexed { index, chip ->
+        if (chip.label.asString(context) == triggers[index].label.asString(context)) {
+            chip.state = triggers[index].state
+        }
+    }
+    return uiChips
+}
+
+fun ConditionLogModel.getWeatherTriggerUiChips(context: Context): List<UiChip> {
+    val uiChips = UiTrigger.weatherTriggerChips
+    val triggers = this.weatherTriggers.map {
+        UiChip(
+            icon = null,
+            label = UiText.DynamicString(it.key),
+            state = it.value.toUiChipState()
+        )
+    }
+    uiChips.forEachIndexed { index, chip ->
+        if (chip.label.asString(context) == triggers[index].label.asString(context)) {
+            chip.state = triggers[index].state
+        }
+    }
+    return uiChips
+}
+
+fun ConditionLogModel.getMentalHealthTriggerUiChips(context: Context): List<UiChip> {
+    val uiChips = UiTrigger.mentalTriggerChips
+    val triggers = this.mentalHealthTriggers.map {
+        UiChip(
+            icon = null,
+            label = UiText.DynamicString(it.key),
+            state = it.value.toUiChipState()
+        )
+    }
+    uiChips.forEachIndexed { index, chip ->
+        if (chip.label.asString(context) == triggers[index].label.asString(context)) {
+            chip.state = triggers[index].state
+        }
+    }
+    return uiChips
+}
+
+fun ConditionLogModel.getOtherTriggerUiChips(context: Context): List<UiChip> {
+    val uiChips = UiTrigger.otherTriggerChips
+    val triggers = this.otherTriggers.map {
+        UiChip(
+            icon = null,
+            label = UiText.DynamicString(it.key),
+            state = it.value.toUiChipState()
+        )
+    }
+    uiChips.forEachIndexed { index, chip ->
+        if (chip.label.asString(context) == triggers[index].label.asString(context)) {
+            chip.state = triggers[index].state
+        }
+    }
+    return uiChips
 }
