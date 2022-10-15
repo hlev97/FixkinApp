e773d0140d69e927a796116930a0e27cac5172cb
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-15 14:41:28 +0200

    Add converters to Feeling and add FeelingUI

--- core/model/src/main/java/hu/bme/aut/it9p0z/model/feeling/Feeling.kt
+++ core/model/src/main/java/hu/bme/aut/it9p0z/model/feeling/Feeling.kt
@@ -28,5 +28,26 @@ sealed class Feeling(val name: String) {
                 else -> Sad
             }
         }
+
+        fun Feeling.asFloat(): Float {
+            return when(this) {
+                is Sad -> 0f
+                is Unhappy -> 1f
+                is Neutral -> 2f
+                is Happy -> 3f
+                is Joyful -> 4f
+            }
+        }
+
+        fun Float.asFeeling(): Feeling {
+            return when(this) {
+                0f -> Sad
+                1f -> Unhappy
+                2f -> Neutral
+                3f -> Happy
+                4f -> Joyful
+                else -> Sad
+            }
+        }
     }
 }
---
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/ui/data/Feelings.kt
@@ -0,0 +1,32 @@
+package hu.bme.aut.it9p0z.ui.data
+
+import hu.bme.aut.it9p0z.ui.R
+import hu.bme.aut.it9p0z.ui.model.UiIcon
+import hu.bme.aut.it9p0z.ui.model.UiText
+
+sealed class FeelingUi(val name: UiText, val icon: UiIcon?) {
+    object Sad: FeelingUi(
+        name = UiText.StringResource(id = R.string.feeling_name_sad),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_sad_24)
+    )
+    object Unhappy: FeelingUi(
+        name = UiText.StringResource(id = R.string.feeling_name_unhappy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_unhappy_24)
+    )
+    object Neutral: FeelingUi(
+        name = UiText.StringResource(id = R.string.feeling_name_neutral),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_neutral_24)
+    )
+    object Happy: FeelingUi(
+        name = UiText.StringResource(id = R.string.feeling_name_happy),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_happy_24)
+    )
+    object Joyful: FeelingUi(
+        name = UiText.StringResource(id = R.string.feeling_name_joyful),
+        icon = UiIcon.ImageResource(id = R.drawable.ic_joyful_24)
+    )
+
+    companion object {
+        val feelings = listOf(Sad,Unhappy,Neutral,Happy,Joyful)
+    }
+}
