4ddc46d56daf0eb072afed4df00a777d497e8ad0
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-15 14:42:47 +0200

    Fix chip group

--- core/ui/src/main/java/hu/bme/aut/it9p0z/ui/components/chipgroup/ChipGroup.kt
+++ core/ui/src/main/java/hu/bme/aut/it9p0z/ui/components/chipgroup/ChipGroup.kt
@@ -11,10 +11,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.flowlayout.FlowMainAxisAlignment
 import com.google.accompanist.flowlayout.FlowRow
 import hu.bme.aut.it9p0z.ui.components.chipgroup.ChipGroupDefaults.CHIP_GROUP_TITLE_MAX_LINES
 import hu.bme.aut.it9p0z.ui.model.UiChip
+import hu.bme.aut.it9p0z.ui.model.UiChip.Companion.toBoolean
 import hu.bme.aut.it9p0z.ui.model.UiIcon
 import hu.bme.aut.it9p0z.ui.model.UiText
 import hu.bme.aut.it9p0z.ui.theme.FixkinTheme
@@ -23,10 +25,11 @@ import hu.bme.aut.it9p0z.ui.theme.dp_s
 
 @ExperimentalMaterial3Api
 @Composable
-fun ColumnScope.ChipGroup(
+fun ChipGroup(
     label: UiText?,
     chips: List<UiChip>,
     modifier: Modifier = Modifier,
+    enabled: Boolean = true,
 ) {
     val context = LocalContext.current
     if (label != null) {
@@ -42,23 +45,33 @@ fun ColumnScope.ChipGroup(
     }
     FlowRow(
         modifier = modifier.fillMaxWidth(0.95f),
-        mainAxisAlignment = FlowMainAxisAlignment.SpaceAround,
+        mainAxisAlignment = FlowMainAxisAlignment.SpaceBetween,
         lastLineMainAxisAlignment = FlowMainAxisAlignment.Start
     ) {
        chips.forEach { chip ->
-           var selected by remember { mutableStateOf(false) }
+           var selected by remember { mutableStateOf(chip.state.toBoolean()) }
            ElevatedFilterChip(
                selected = selected,
                onClick = {
                    chip.onChangeState()
                    selected = chip.state == UiChip.UiChipState.SELECTED
                  },
-               enabled = chip.enabled,
+               enabled = enabled,
                label = { Text(text = chip.label.asString(context))},
                leadingIcon = if (chip.state == UiChip.UiChipState.SELECTED) {
-                   { Icon(imageVector = Icons.Rounded.Check, contentDescription = null) }
+                   {
+                       Icon(
+                           imageVector = Icons.Rounded.Check,
+                           contentDescription = null,
+                           modifier = Modifier.size(20.dp)
+                       )
+                   }
                } else {
-                   { chip.icon?.AsImage() }
+                   {
+                       chip.icon?.AsImage(
+                           modifier = modifier.size(20.dp)
+                       )
+                   }
                },
                modifier = Modifier.padding(dp_s)
            )
