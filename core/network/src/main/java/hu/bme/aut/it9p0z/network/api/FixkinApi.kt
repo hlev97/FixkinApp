07651bc5708b52eb42c46df59480b6698cbf451b
Heizer Levente <60049137+hlev97@users.noreply.github.com>
2022-10-22 15:24:46 +0200

    Add new DTO and API requests for survey

--- core/network/src/main/java/hu/bme/aut/it9p0z/network/api/FixkinApi.kt
+++ core/network/src/main/java/hu/bme/aut/it9p0z/network/api/FixkinApi.kt
@@ -2,6 +2,7 @@ package hu.bme.aut.it9p0z.network.api
 
 import hu.bme.aut.it9p0z.network.dtos.ConditionLogDto
 import hu.bme.aut.it9p0z.network.dtos.ConditionLogStatisticsDto
+import hu.bme.aut.it9p0z.network.dtos.SurveyLogDto
 import hu.bme.aut.it9p0z.network.dtos.UserDto
 import retrofit2.Response
 import retrofit2.http.Body
@@ -53,4 +54,15 @@ interface FixkinApi {
     suspend fun getStatistics(
         @Header("Authorization") credentials: String
     ): Response<ConditionLogStatisticsDto>
+
+    @POST("survey_logs")
+    suspend fun createSurveyLog(
+        @Header("Authorization") credentials: String,
+        @Body log: SurveyLogDto
+    ): Response<SurveyLogDto>
+
+    @GET("survey_logs/all/me")
+    suspend fun getAllSurveyLogs(
+        @Header("Authorization") credentials: String,
+    ): Response<List<SurveyLogDto>>
 }
---
+++ core/network/src/main/java/hu/bme/aut/it9p0z/network/dtos/SurveyLogDto.kt
@@ -0,0 +1,26 @@
+package hu.bme.aut.it9p0z.network.dtos
+
+import com.squareup.moshi.JsonClass
+import hu.bme.aut.it9p0z.model.surveylog.SurveyLogModel
+import java.time.LocalDate
+
+@JsonClass(generateAdapter = true)
+data class SurveyLogDto(
+    val surveyLogId: Int,
+    val userName: String?,
+    val creationDate: LocalDate,
+    val result: Double,
+)
+
+fun SurveyLogModel.asSurveyLogDto(): SurveyLogDto = SurveyLogDto(
+    surveyLogId = id,
+    userName = null,
+    creationDate = creationDate,
+    result = result
+)
+
+fun SurveyLogModel.asSurveyLogModel(): SurveyLogModel = SurveyLogModel(
+    id = id,
+    creationDate = creationDate,
+    result = result
+)
